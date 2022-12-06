e4a8e94b1d4c5e91851e07daba3e5c650bd452c9
azierariel <ariel-azi@hotmail.com>
2022-12-06 22:42:38 +0100

    add day 3 solution

--- day_3.py
+++ day_3.py
@@ -13,11 +13,26 @@ for string in input.split('\n')[:-1]:
     item = re.search(f"[{string[:mid]}]", string[mid:]).group(0)
     items.append(item)
 
-scores = 'abcdefghijklmnopqrstuvwxyz'
 
-priorities_sum = 0
-for item in items:
-    if item in scores:
-        priorities_sum += scores.index(item) + 1
-    else:
-        priorities_sum += scores.upper().index(item) + 27
+def calc_priorities(items: list[str]) -> int:
+    scores = 'abcdefghijklmnopqrstuvwxyz'
+    priorities_sum = 0
+    for item in items:
+        if item in scores:
+            priorities_sum += scores.index(item) + 1
+        else:
+            priorities_sum += scores.upper().index(item) + 27
+    return priorities_sum
+
+
+### part 2
+sacks = input.split('\n')[:-1]
+items = []
+sacks_n = len(sacks)
+for i in range(0,sacks_n,3):
+    string = '@'.join(sacks[i:i+3])
+    
+    item = re.search(r"(.).*@.*(\1).*@.*(\1)", string).group(1)
+    items.append(item)
+
+priorities = calc_priorities(items)
